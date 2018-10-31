0122f258cf8c4dc9eac0127bf1cea773e5007e6b
aminhidayatt <43781728+aminhidayatt@users.noreply.github.com>
2018-10-31 21:24:59 +0700

    Create  MainQueueString.java

---
+++ MainQueueString.java
@@ -0,0 +1,14 @@
+import sun.misc.Queue;
+public class MainQueueString {
+
+	public static void main(String[] args) {
+		QueueString q = new QueueString();
+		q.cetak();
+		
+		q.insert("yayat");q.cetak();
+		q.insert("amin");q.cetak();
+		q.insert("dayat");q.cetak();
+		q.insert("maman");q.cetak();
+
+	}
+}
